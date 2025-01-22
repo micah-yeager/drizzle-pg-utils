21dd78456256ff2d9d08e71384f234992f247801
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-01-22 15:18:30 -0500

    docs(vendor): add permalink to function source

--- vendor/drizzle-orm/utils.ts
+++ vendor/drizzle-orm/utils.ts
@@ -1,3 +1,4 @@
+/** @see https://github.com/drizzle-team/drizzle-orm/blob/49d2930126170087e472d98f6278fa2f3e8279d4/drizzle-orm/src/utils.ts#L234-L242 */
 export function getColumnNameAndConfig<
 	TConfig extends Record<string, any> | undefined,
 >(a: string | TConfig | undefined, b: TConfig | undefined) {
