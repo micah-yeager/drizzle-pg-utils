88b464777c7787d196e5f53c02327d19d9aececc
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-01-23 13:27:53 -0500

    style(func): remove unnecessary `unknown` type default

--- lib/functions/json/to-json.ts
+++ lib/functions/json/to-json.ts
@@ -1,6 +1,6 @@
 import { type SQL, type SQLWrapper, sql } from "drizzle-orm"
 
 /** @see https://www.postgresql.org/docs/current/functions-json.html#FUNCTIONS-JSON-CREATION-TABLE */
-export function toJson<TType = unknown>(expression: SQLWrapper): SQL<TType> {
+export function toJson<TType>(expression: SQLWrapper): SQL<TType> {
 	return sql`to_json(${expression})`
 }
--- lib/functions/json/to-jsonb.ts
+++ lib/functions/json/to-jsonb.ts
@@ -2,6 +2,6 @@ import type { SQLWrapper } from "drizzle-orm"
 import { type SQL, sql } from "drizzle-orm"
 
 /** @see https://www.postgresql.org/docs/current/functions-json.html#FUNCTIONS-JSON-CREATION-TABLE */
-export function toJsonb<TType = unknown>(expression: SQLWrapper): SQL<TType> {
+export function toJsonb<TType>(expression: SQLWrapper): SQL<TType> {
 	return sql`to_jsonb(${expression})`
 }
