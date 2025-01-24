f7350f4c1bc56f8fc322e7bcef49bc0f3b0ab86c
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-01-24 15:54:26 -0500

    test(func): move column builder testing to appropriate helper type test

--- lib/functions/cast.test-d.ts
+++ lib/functions/cast.test-d.ts
@@ -11,14 +11,14 @@ it("should return as unknown if using string to cast", () => {
 	expectTypeOf(cast(sql``, "text")).toEqualTypeOf<SQL<unknown>>()
 })
 
-describe("return type override", () => {
-	it("should return correctly for unknown types", () => {
-		expectTypeOf(cast<TestJson>(sql``, json)).toEqualTypeOf<SQL<TestJson>>()
-	})
+it("should return as type if using column builder", () => {
+	expectTypeOf(cast(sql``, integer())).toEqualTypeOf<SQL<number>>()
+})
 
-	it("should return correctly for inferred types", () => {
-		expectTypeOf(cast<number>(sql``, text)).toEqualTypeOf<SQL<number>>()
-	})
+it("should return as type if using table column", () => {
+	const table = pgTable("", { col: integer() })
+
+	expectTypeOf(cast(sql``, table.col)).toEqualTypeOf<SQL<number>>()
 })
 
 describe("column builder function", () => {
@@ -35,71 +35,12 @@ describe("column builder function", () => {
 	})
 })
 
-describe("column builder", () => {
-	it("should return correctly for built-in type", () => {
-		expectTypeOf(cast(sql``, integer())).toEqualTypeOf<SQL<number>>()
+describe("return type override", () => {
+	it("should return correctly for unknown types", () => {
+		expectTypeOf(cast<TestJson>(sql``, json)).toEqualTypeOf<SQL<TestJson>>()
 	})
 
-	it("should return correctly for custom type from `customType()`", () => {
-		expectTypeOf(cast(sql``, money())).toEqualTypeOf<SQL<string>>()
-	})
-
-	it("should return correctly for custom type from `PgColumnBuilder`", () => {
-		expectTypeOf(cast(sql``, circle())).toEqualTypeOf<SQL<CircleTuple>>()
-	})
-
-	it("should return correctly for array", () => {
-		expectTypeOf(cast(sql``, integer().array())).toEqualTypeOf<SQL<number[]>>()
-	})
-
-	it("should return correctly for `$type()`", () => {
-		expectTypeOf(cast(sql``, json().$type<TestJson>())).toEqualTypeOf<
-			SQL<TestJson>
-		>()
-	})
-
-	it("should return correctly for enum", () => {
-		expectTypeOf(
-			cast(sql``, text("enum", { enum: ["enum1", "enum2"] }).array()),
-		).toEqualTypeOf<SQL<("enum1" | "enum2")[]>>()
-	})
-})
-
-describe("table column", () => {
-	const table = pgTable("", {
-		builtIn: integer(),
-		customType: money(),
-		columnBuilder: circle(),
-		array: integer().array(),
-		typed: json().$type<TestJson>(),
-		enum: text("enum", { enum: ["enum1", "enum2"] }).array(),
-	})
-
-	it("should return correctly for built-in type", () => {
-		expectTypeOf(cast(sql``, table.builtIn)).toEqualTypeOf<SQL<number>>()
-	})
-
-	it("should return correctly for custom type from `customType()`", () => {
-		expectTypeOf(cast(sql``, table.customType)).toEqualTypeOf<SQL<string>>()
-	})
-
-	it("should return correctly for custom type from `PgColumnBuilder`", () => {
-		expectTypeOf(cast(sql``, table.columnBuilder)).toEqualTypeOf<
-			SQL<CircleTuple>
-		>()
-	})
-
-	it("should return correctly for array", () => {
-		expectTypeOf(cast(sql``, table.array)).toEqualTypeOf<SQL<number[]>>()
-	})
-
-	it("should return correctly for `$type()`", () => {
-		expectTypeOf(cast(sql``, table.typed)).toEqualTypeOf<SQL<TestJson>>()
-	})
-
-	it("should return correctly for enum", () => {
-		expectTypeOf(cast(sql``, table.enum)).toEqualTypeOf<
-			SQL<("enum1" | "enum2")[]>
-		>()
+	it("should return correctly for inferred types", () => {
+		expectTypeOf(cast<number>(sql``, text)).toEqualTypeOf<SQL<number>>()
 	})
 })
--- lib/utils/type-from-sql.test-d.ts
+++ lib/utils/type-from-sql.test-d.ts
@@ -1,78 +1,140 @@
+// noinspection JSUnusedLocalSymbols
+
 import type { SQL } from "drizzle-orm"
-import { integer, json, pgTable, text } from "drizzle-orm/pg-core"
+import { integer, json, jsonb, pgTable, text } from "drizzle-orm/pg-core"
 import { describe, expectTypeOf, it } from "vitest"
-import type { TypeFromSQL } from "./util"
+import { type CircleTuple, circle } from "../columns/circle"
+import { money } from "../columns/money"
+import type { TypeFromSQL } from "./type-from-sql"
 
-describe("TypeFromSQL", () => {
-	describe("column", () => {
-		it("should return the correct type when untyped", () => {
-			// eslint-disable-next-line unused-imports/no-unused-vars
-			const tests = pgTable("tests", {
-				text: text("text"),
-				integer: integer("integer").notNull(),
-				json: json("json"),
-			})
+type JsonType = { test: string; nested: { key: "value" } }
 
-			expectTypeOf<TypeFromSQL<typeof tests.text>>().toEqualTypeOf<
-				string | null
-			>()
-			expectTypeOf<TypeFromSQL<typeof tests.integer>>().toEqualTypeOf<number>()
-			expectTypeOf<TypeFromSQL<typeof tests.json>>().toEqualTypeOf<unknown>()
-		})
+describe("column builder", () => {
+	it("should parse built-in type", () => {
+		const builder = integer()
 
-		it("should return the correct type when typed", () => {
-			type TestString = "literal"
-			type TestInteger = 0
-			type TestJson = { test: string }
-			// eslint-disable-next-line unused-imports/no-unused-vars
-			const tests = pgTable("tests", {
-				text: text("text").$type<TestString>(),
-				integer: integer("integer").notNull().$type<TestInteger>(),
-				json: json("json").$type<TestJson>(),
-			})
-
-			expectTypeOf<
-				TypeFromSQL<typeof tests.text>
-			>().toEqualTypeOf<TestString | null>()
-			expectTypeOf<
-				TypeFromSQL<typeof tests.integer>
-			>().toEqualTypeOf<TestInteger>()
-			expectTypeOf<
-				TypeFromSQL<typeof tests.json>
-			>().toEqualTypeOf<TestJson | null>()
-		})
+		expectTypeOf<TypeFromSQL<typeof builder>>().toEqualTypeOf<number | null>()
 	})
 
-	describe("SQL", () => {
-		it("should return the correct type when untyped", () => {
-			expectTypeOf<TypeFromSQL<SQL>>().toEqualTypeOf<unknown>()
-		})
+	it("should parse custom type from `customType()`", () => {
+		const builder = money()
 
-		it("should return the correct type when typed", () => {
-			type TestString = "literal"
-			type TestInteger = 0
-			type TestJson = { test: string }
-
-			expectTypeOf<TypeFromSQL<SQL<TestString>>>().toEqualTypeOf<TestString>()
-			expectTypeOf<TypeFromSQL<SQL<TestInteger>>>().toEqualTypeOf<TestInteger>()
-			expectTypeOf<TypeFromSQL<SQL<TestJson>>>().toEqualTypeOf<TestJson>()
-		})
+		expectTypeOf<TypeFromSQL<typeof builder>>().toEqualTypeOf<string | null>()
 	})
 
-	describe("column builder", () => {
-		it("should return the correct type when untyped", () => {
-			// eslint-disable-next-line unused-imports/no-unused-vars
-			const textBuilder = text("text")
-			// eslint-disable-next-line unused-imports/no-unused-vars
-			const integerBuilder = integer("integer").notNull()
-			// eslint-disable-next-line unused-imports/no-unused-vars
-			const jsonBuilder = json("json")
+	it("should parse custom type from `PgColumnBuilder`", () => {
+		const builder = circle()
 
-			expectTypeOf<TypeFromSQL<typeof textBuilder>>().toEqualTypeOf<
-				string | null
-			>()
-			expectTypeOf<TypeFromSQL<typeof integerBuilder>>().toEqualTypeOf<number>()
-			expectTypeOf<TypeFromSQL<typeof jsonBuilder>>().toEqualTypeOf<unknown>()
-		})
+		expectTypeOf<
+			TypeFromSQL<typeof builder>
+		>().toEqualTypeOf<CircleTuple | null>()
+	})
+
+	it("should parse when using `.notNull()`", () => {
+		const builder = circle().notNull()
+
+		expectTypeOf<TypeFromSQL<typeof builder>>().toEqualTypeOf<CircleTuple>()
+	})
+
+	it("should parse when using `.array()`", () => {
+		const builder = circle().array()
+
+		expectTypeOf<TypeFromSQL<typeof builder>>().toEqualTypeOf<
+			CircleTuple[] | null
+		>()
+	})
+
+	it("should parse when using `.$type()`", () => {
+		const builder = jsonb().$type<JsonType>()
+
+		expectTypeOf<TypeFromSQL<typeof builder>>().toEqualTypeOf<JsonType | null>()
+	})
+
+	it("should prefer `.$type()` over built-in type", () => {
+		const builder = text().$type<number>()
+
+		expectTypeOf<TypeFromSQL<typeof builder>>().toEqualTypeOf<number | null>()
+	})
+
+	it("should allow unknown types", () => {
+		const builder = jsonb()
+
+		expectTypeOf<TypeFromSQL<typeof builder>>().toEqualTypeOf<unknown>()
+	})
+})
+
+describe("table column", () => {
+	// biome-ignore lint/correctness/noUnusedVariables: Only used for type info.
+	const table = pgTable("", {
+		builtIn: integer(),
+		customType: money(),
+		columnBuilder: circle(),
+		array: integer().array(),
+		typed: json().$type<JsonType>(),
+		enum: text({ enum: ["enum1", "enum2"] }).array(),
+	})
+
+	it("should parse built-in type", () => {
+		const table = pgTable("", { col: integer() })
+
+		expectTypeOf<TypeFromSQL<typeof table.col>>().toEqualTypeOf<number | null>()
+	})
+
+	it("should parse custom type from `customType()`", () => {
+		const table = pgTable("", { col: money() })
+
+		expectTypeOf<TypeFromSQL<typeof table.col>>().toEqualTypeOf<string | null>()
+	})
+
+	it("should parse custom type from `PgColumnBuilder`", () => {
+		const table = pgTable("", { col: circle() })
+
+		expectTypeOf<
+			TypeFromSQL<typeof table.col>
+		>().toEqualTypeOf<CircleTuple | null>()
+	})
+
+	it("should parse when using `.notNull()`", () => {
+		const table = pgTable("", { col: circle().notNull() })
+
+		expectTypeOf<TypeFromSQL<typeof table.col>>().toEqualTypeOf<CircleTuple>()
+	})
+
+	it("should parse when using `.array()`", () => {
+		const table = pgTable("", { col: circle().array() })
+
+		expectTypeOf<TypeFromSQL<typeof table.col>>().toEqualTypeOf<
+			CircleTuple[] | null
+		>()
+	})
+
+	it("should parse when using `.$type()`", () => {
+		const table = pgTable("", { col: jsonb().$type<JsonType>() })
+
+		expectTypeOf<
+			TypeFromSQL<typeof table.col>
+		>().toEqualTypeOf<JsonType | null>()
+	})
+
+	it("should prefer `.$type()` over built-in type", () => {
+		const table = pgTable("", { col: text().$type<"test">() })
+
+		expectTypeOf<TypeFromSQL<typeof table.col>>().toEqualTypeOf<"test" | null>()
+	})
+
+	it("should allow unknown types", () => {
+		const table = pgTable("", { col: jsonb() })
+
+		expectTypeOf<TypeFromSQL<typeof table.col>>().toEqualTypeOf<unknown>()
+	})
+})
+
+describe("SQL", () => {
+	it("should parse when untyped", () => {
+		expectTypeOf<TypeFromSQL<SQL>>().toEqualTypeOf<unknown>()
+	})
+
+	it("should parse when typed", () => {
+		expectTypeOf<TypeFromSQL<SQL<JsonType>>>().toEqualTypeOf<JsonType>()
 	})
 })
