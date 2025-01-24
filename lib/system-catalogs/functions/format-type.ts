8808b32d4495a8f7fde2dab783f843fb5cfdf1a6
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-01-24 16:01:53 -0500

    docs(func): add missing inline TSDoc

--- lib/system-catalogs/functions/format-type.ts
+++ lib/system-catalogs/functions/format-type.ts
@@ -1,5 +1,6 @@
 import { type SQL, type SQLWrapper, sql } from "drizzle-orm"
 
+/** @see https://www.postgresql.org/docs/17/functions-info.html#FUNCTIONS-INFO-CATALOG-TABLE */
 export function formatType(oid: SQLWrapper, typemod: SQLWrapper): SQL<string> {
 	return sql`format_type(${oid}, ${typemod})`
 }
